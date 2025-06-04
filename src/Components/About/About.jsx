c9333c1aa572781b33a5ee92d93af9330f373545
Kamran Afzal Khan <kamranafzalkhan29@gmail.com>
2025-06-04 21:48:53 +0530

    updating Yrs of experience

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -12,9 +12,9 @@ const About = () => {
               About <span>me</span>
             </h1>
             <h2>
-              Networking Analyst
+              Network Architecture Analyst
             </h2>
-            <p>I am a skilled Networking Analyst with over 3 years of exprience in the industry. My passion lies in <br/>creating capturing networking architecture and implementing them through networking tools. I take <br/> pride in staying up-to-date with current networking trends.Throught my career, I have devloped a <br/>deep understanding of networking concepts. When a starting a new project I thoroughly research <br/>and analyze the given problem statement and the client specific requirements. </p>
+            <p>I am a skilled Networking Analyst with over 4 years of exprience in the industry. My passion lies in <br/>creating capturing networking architecture and implementing them through networking tools. I take <br/> pride in staying up-to-date with current networking trends.Throught my career, I have devloped a <br/>deep understanding of networking concepts. When a starting a new project I thoroughly research <br/>and analyze the given problem statement and the client specific requirements. </p>
         </div>
     </div>
   )
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
         <div className="hero-left">
         <h2>Hello, It's Me</h2>
         <h1>Ghausia Afzal</h1>
-        <h2>And I'm a <span>Networking Analyst</span></h2>
-        <p>I,m a Networking Analyst with extensive experience for over 3 years.<br/>Expertise is to create records in both Ultra DNS and Infobox, L2/L3 <br/>Troubleshooting and many more.......</p>
+        <h2>And I'm a <span>Network Architecture Analyst</span></h2>
+        <p>I,m a Networking Analyst with extensive experience for over 4 years.<br/>Expertise is to create records in both Ultra DNS and Infobox, L2/L3 <br/>Troubleshooting and many more.......</p>
         <div className="refrels">
             <a href="https://m.facebook.com/profile.php?id=100012100906873"><i className='bx bxl-meta'></i></a>
             <a href="https://www.instagram.com/ghausiaafzal?igsh=MTRrMnpyNG5vMnk0YQ=="><i className='bx bxl-instagram' ></i></a>
